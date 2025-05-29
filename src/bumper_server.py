1827c9c01e38c9fa1def2cc668d9e35ab0cec177
Obscuriosity <tony.obscuriosity@gmail.com>
2025-05-29 17:24:24 +0100

    adjusting

--- src/bumper_server.py
+++ src/bumper_server.py
@@ -43,18 +43,20 @@ class bumperServer(object):
                 self._as.set_preempted()
                 success = False
                 break
-            percent += 10
-            self._feedback.percent_complete = percent
-            self._as.publish_feedback(self._feedback)
-            if (percent == 100):
-                moving = False
-            # for testing, remove later
+            self.count()
             r.sleep()
 
         if success:
             self._result.Done = True
             rospy.loginfo('%s: Succeeded' % self._action_name)
             self._as.set_succeeded(self._result)
+
+    def count(self):
+        percent += 10
+        self._feedback.percent_complete = percent
+        self._as.publish_feedback(self._feedback)
+        if (percent == 100):
+            moving = False
 
 
 if __name__ == '__main__':
